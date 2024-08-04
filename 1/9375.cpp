5594974cae1cc7163b8567e498746662013f5bee
EricJang <ericjangdev@gmail.com>
2024-08-04 11:32:17 +0900

    9275

---
+++ 1/9375.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int testcase, inputN; 
+    string clothName, clothCategory;
+
+    cin >> testcase;
+    while(testcase--)
+    {
+        cin >> inputN;
+        map<string, int> cloth;
+        int ret = 1;
+
+        // map 채우기
+        for(int i = 0; i < inputN; i++)
+        {
+            cin >> clothName >> clothCategory;
+            cloth[clothCategory]++;
+        }
+
+        // (의상 종류별 개수 + 1)끼리 곱하고 - 1
+        for(pair i : cloth)
+        {
+            ret *= (i.second + 1);
+        }
+
+        cout << ret - 1;
+    }
+}
